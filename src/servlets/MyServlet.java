b9b3617e40e24354d757137f24f6fc2c395e883d
gaolongf <2017214030@stu.cqupt.edu.cn>
2018-12-14 11:46:15 +0800

    level0

---
+++ src/servlets/MyServlet.java
@@ -0,0 +1,32 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet
+{
+    private String message;
+    @Override
+    public void init() throws ServletException
+    {
+        message="hello world";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter pw=resp.getWriter();
+        pw.println(message);
+        pw.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
